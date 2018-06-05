a089ad46353b6c1dc889a308021302115f0e7277
Jeana Smith <jelsmith@cspire.com>
2018-06-05 15:43:37 -0500

    Convert events html to a template that will auto populate based on the angular list of events

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular.module('cSpireGamingWebApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch']).config(function ($routeProvider) {
+angular.module('cSpireGamingWebApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch']).config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'views/main.html',
@@ -18,4 +18,11 @@ angular.module('cSpireGamingWebApp', ['ngAnimate', 'ngCookies', 'ngResource', 'n
         .otherwise({
             redirectTo: '/'
         });
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'https://www.google.com/**'
+  ]);
 });
