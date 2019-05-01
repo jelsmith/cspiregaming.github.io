0c3b5e471b992088be753fbaff1bd510fcea62f9
Jeana Smith <jelsmith@cspire.com>
2019-05-01 09:39:18 -0500

    Add Chris Ellis. Remove Ryan Carey.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -89,18 +89,14 @@ angular.module('cSpireGamingWebApp').controller('MainCtrl', function ($scope, $f
         image: 'rashad-collier.png',
         about: `Coding, tech knowledge and everything nerd; these were the ingredients chosen to create the perfect software developer. But Professor Utonium accidentally added an extra ingredient to the concoction: video games. Thus, <strong>Rashad Collier</strong> was born! Using his ultra-super powers, <strong>Rashad</strong> has dedicated his life to smash at fighting games and the forces of evil.`
     }, {
-        name: 'Ryan<br>Carey',
-        image: 'ryan-carey.png',
-        about: `<strong>Ryan Carey</strong> is a superhero whose abilities include levitating inches off the ground, turning his nose invisible, and transforming into a single cheeto.`
-    }, {
         name: 'Tommy<br>Naugle',
         image: 'tommy-naugle.png',
         about: `<strong>Tommy Naugle</strong> is a data scientist at C Spire and Mississippi's favorite TensorBro. He enjoys playing with Pokemon cards when his wife allows it.`
-    },/* {
-       // name: 'Chris<br>Ellis',
-     //   image: 'chris-ellis.png',
-       // about: `<strong>Chris Ellis</strong> By day, Chris Ellis is a Sales Operations Specialist. By night, he maintains a 1:47 KDR (Hey, we said these people LOVED gaming, not that they were particularly good at it). <strong>Chris Ellis</strong> is one of those weirdos who actually enjoys playing Monopoly. He would love to tell you about his cats.`
-    }, */{
+    }, {
+        name: 'Chris<br>Ellis',
+        image: 'chris-ellis.jpg',
+        about: `By day, <strong>Chris Ellis</strong> is a Sales Operations Specialist. By night, he maintains a 1:47 KDR (Hey, we said these people LOVED gaming, not that they were particularly good at it). <strong>Chris Ellis</strong> is one of those weirdos who actually enjoys playing Monopoly. He would love to tell you about his cats.`
+    }, {
         name: 'Rob<br>Jones',
         image: 'rob-jones.jpeg',
         about: `“I am the hope of the universe; I am the hope of all living things that cry out for peace; I am protector of the innocent; I am the light in the darkness; I am truth. Ally to good — nightmare to people who resist cord cutting and amazing 1Gbps internet speeds provided by C Spire.” - <strong>Rob Jones </strong>`
